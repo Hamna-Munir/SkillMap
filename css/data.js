cda40d2617bb357cc16955b75d27bd4ad22163b0
Hamna-Munir <hamnamunir27@gmail.com>
2026-01-07 19:45:58 +0500

    Create data.js

---
+++ css/data.js
@@ -0,0 +1,41 @@
+const skillData = {
+  "web-developer": {
+    title: "Web Developer",
+    description: "Learn front-end and back-end development step by step.",
+    skills: [
+      {
+        name: "HTML",
+        description: "Learn the structure of web pages using HTML.",
+        resources: [
+          { name: "MDN HTML Guide", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+          { name: "FreeCodeCamp HTML", link: "https://www.freecodecamp.org/learn" }
+        ]
+      },
+      {
+        name: "CSS",
+        description: "Style your web pages using CSS.",
+        resources: [
+          { name: "MDN CSS Guide", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+          { name: "CSS Tricks", link: "https://css-tricks.com/" }
+        ]
+      },
+      {
+        name: "JavaScript",
+        description: "Add interactivity with JavaScript.",
+        resources: [
+          { name: "JavaScript.info", link: "https://javascript.info/" },
+          { name: "FreeCodeCamp JS", link: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/" }
+        ]
+      }
+    ]
+  },
+  "data-scientist": {
+    title: "Data Scientist",
+    description: "Learn Python, ML, and data visualization.",
+    skills: [
+      { name: "Python", description: "Learn programming with Python.", resources: [{ name: "Python Official Docs", link: "https://docs.python.org/3/" }] },
+      { name: "Pandas", description: "Data manipulation library.", resources: [{ name: "Pandas Docs", link: "https://pandas.pydata.org/" }] },
+      { name: "Matplotlib", description: "Data visualization library.", resources: [{ name: "Matplotlib Docs", link: "https://matplotlib.org/" }] }
+    ]
+  }
+};
